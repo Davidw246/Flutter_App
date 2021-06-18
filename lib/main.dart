7b9fb20b04be55e1c9eef50786265f7a63352650
David Willian <75228364+Davidw246@users.noreply.github.com>
2021-06-18 11:19:48 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[600],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[600],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
